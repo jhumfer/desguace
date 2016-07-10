5e1ae67c49ece95e9980a0b79355fe6d3e218c43
Rafa <rafarastrero.46@gmail.com>
2016-07-10 13:50:17 +0200

    Nuevos modulos para la web

---
+++ jar_desguace_web/controllers/main.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+# -*- coding: utf-8 -*-
+import base64
+
+import werkzeug
+import werkzeug.urls
+
+from openerp import http, SUPERUSER_ID
+from openerp.http import request
+from openerp.tools.translate import _
+
+class somos(http.Controller):
+
+    @http.route(['/page/services'], type='http', auth="public", website=True)
+    def solicitud(self, **kwargs):
+        values = {}
+        return request.website.render("website.services", values)
